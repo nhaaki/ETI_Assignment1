211ecefffead455ccabf710ba0b23abe0f14f781
nhaaki <S10206177@connect.np.edu.sg>
2022-12-10 18:23:04 +0800

    Create new DBs and distribute the tables

--- AssignmentSQLScriptSetup.sql
+++ AssignmentSQLScriptSetup.sql
@@ -5,5 +5,9 @@ CREATE DATABASE DriveUserDB;
 USE DriveUserDB;
 CREATE TABLE Passengers (UserID INT auto_increment, Username varchar(50), Password varchar(50), FirstName varchar(20), LastName varchar(20), MobileNo varchar(8), EmailAddress varchar(50), PRIMARY KEY (UserID));
 CREATE TABLE Drivers (UserID INT auto_increment, Username varchar(50), Password varchar(50), FirstName varchar(20), LastName varchar(20), MobileNo varchar(8), EmailAddress varchar(50), IdNo varchar(20), CarLicenseNo varchar(20), PRIMARY KEY (UserID));
-CREATE TABLE LiveRides (driverUID INT, passengerUID INT, pcPickUp varchar(10), pcDropOff varchar(10), status varchar(15), primary key (DriverUID))
+CREATE DATABASE DriveFunctionDB;
+USE DriveFunctionDB;
+CREATE TABLE LiveRides (driverUID INT, passengerUID INT, pcPickUp varchar(10), pcDropOff varchar(10), status varchar(15), primary key (DriverUID));
+CREATE DATABASE DriveDataDB;
+USE DriveDataDB;
 CREATE TABLE RideHistory (passengerUID INT, driverUID INT, pcPickUp varchar(10), pcDropOff varchar(10), rideDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);
